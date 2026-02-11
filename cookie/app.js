2fce749227cfcbe674749d8d07acc4722f62bc33
Shivam <Shivamjogdand2580@gmail.com>
2026-02-11 20:23:19 +0530

    revision of cookies and modidified app.js in cookie folder

--- cookie/app.js
+++ cookie/app.js
@@ -5,17 +5,22 @@ const app = express();
 app.use(cookieParser());
 
 app.get("/set-cookie", (req, res) => {
-  res.cookie("user", "Shivam", {
+  res.cookie("user1", "Shivam", {
     httpOnly: true,
     maxAge: 60 * 1000 // 1 minute
   });
+  res.cookie("user2", "Mahesh", {
+    httpOnly: true,
+    maxAge: 60 * 1000 // 1 minute
+  });
+
 
   res.send("Cookie set successfully");
 });
 
 app.get("/get-cookie", (req, res) => {
-  const user = req.cookies.user;
-  res.send(`Cookie value: ${user}`);
+  const {user1,user2} = req.cookies;
+  res.send(`Cookie values: ${user1},${user2}`);
 });
 
 app.listen(3000,()=>{
