7e678c2371e9d9b86fe7ad210edc61733d55e9b3
shiva154200 <shivamjogdand1458@gmail.com>
2026-08-23 19:29:45 +0530

    Fix authorization and validation middleware

--- middleware.js
+++ middleware.js
@@ -1,68 +1,77 @@
-const Listing=require("./models/listing")
+const Listing = require("./models/listing");
+const ExpressError = require("./utils/ExpressError");
+const { listingSchema, reviewSchema } = require("./schemavalidation");
+const Review = require("./models/review");
 
-const ExpressError=require("./utils/ExpressError");
-const { listingSchema,reviewSchema} = require("./schemavalidation");
-const Review=require("./models/review")
-
-module.exports.isLoggedIn=(req,res,next)=>{
-    if(!req.isAuthenticated()){
-    req.session.redirectUrl=req.originalUrl;
-        req.flash("error","you must be logged in to create listing! ")
-       return res.redirect("/login")
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error", "You must be logged in to continue!");
+        return res.redirect("/login");
     }
     next();
-}
+};
 
-module.exports.SaveRedirectUrl=(req,res,next)=>{
-    if(req.session.redirectUrl){
-        res.locals.redirectUrl=req.session.redirectUrl;
+module.exports.SaveRedirectUrl = (req, res, next) => {
+    if (req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+        delete req.session.redirectUrl;
     }
     next();
-}
+};
 
+module.exports.isOwner = async (req, res, next) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
 
-module.exports.isOwner=async(req,res,next)=>{
-    let {id}=req.params;
-    let listing =  await Listing .findById(id);
-    if( !((listing.owner._id).equals(res.locals.userinfo._id))){
-        req.flash("error","u dont permission to do this")
-        return res.redirect(`/listings/${id}`)
+    if (!listing) {
+        req.flash("error", "Listing not found!");
+        return res.redirect("/listings");
+    }
+
+    if (!listing.owner.equals(req.user._id)) {
+        req.flash("error", "You don't have permission to do this!");
+        return res.redirect(`/listings/${id}`);
     }
 
     next();
-}
+};
 
-module.exports.validateListing=  (req, res, next) => {
+module.exports.validateListing = (req, res, next) => {
     const { error } = listingSchema.validate(req.body);
 
     if (error) {
-        const msg = error.details.map(el => el.message).join(", ");
-        throw new ExpressError(400, msg);
-    } else {
-        next();
-    }
-};
-
-
- module.exports.validateReview=(req,res,next)=>{
-    const {error }=reviewSchema.validate(req.body);
-     if (error) {
-        const msg = error.details.map(el => el.message).join(", ");
-        throw new ExpressError(400, msg);
-    } else {
-        next();
-    }
-
-    
-}
-
-module.exports.isReviewAuthor=async(req,res,next)=>{
-    let {reviewId,id}=req.params;
-    let review =  await Review .findById(reviewId);
-    if( !((review.author._id).equals(res.locals.userinfo._id))){
-        req.flash("error","u don't have permission to do this")
-        return res.redirect(`/listings/${id}`)
+        const msg = error.details.map((el) => el.message).join(", ");
+        return next(new ExpressError(400, msg));
     }
 
     next();
-}
+};
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+
+    if (error) {
+        const msg = error.details.map((el) => el.message).join(", ");
+        return next(new ExpressError(400, msg));
+    }
+
+    next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { reviewId, id } = req.params;
+    const review = await Review.findById(reviewId);
+
+    if (!review) {
+        req.flash("error", "Review not found!");
+        return res.redirect(`/listings/${id}`);
+    }
+
+    if (!review.author.equals(req.user._id)) {
+        req.flash("error", "You don't have permission to do this!");
+        return res.redirect(`/listings/${id}`);
+    }
+
+    next();
+};
