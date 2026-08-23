d4997205df30131e5827500693f936de58aebdc8
shiva154200 <shivamjogdand1458@gmail.com>
2026-08-23 19:29:25 +0530

    Fix review schema validation and timestamp

--- models/review.js
+++ models/review.js
@@ -1,22 +1,27 @@
-const mongoose = require('mongoose');
-const Schema=mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const reviewSchema=new Schema({
-    comment:String,
-    rating:{
-        type:Number,
-        min:1,
-        max:5
+const reviewSchema = new Schema({
+    comment: {
+        type: String,
+        required: true,
+        trim: true
     },
-    createdAt:{
-        type:Date,
-        default:Date.now()
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
     },
-    author:{
-        type:Schema.Types.ObjectId,
-        ref:"User"
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
     }
-})
+});
 
-module.exports=mongoose.model('Review',reviewSchema);
-  
+module.exports = mongoose.model("Review", reviewSchema);
